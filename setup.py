a928fa1cfc208c24ce2d3c35db97b97de06c9ca1
nviolante25 <nviolante96@gmail.com>
2022-03-22 13:13:30 +0100

    add setup build

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="adain",
-    version="0.1",
-    packages=["src"],
-    install_requires=[
-        "numpy"
-    ],
-    extras_require={
-        "dev": ["black"],
-    },
-)
+setup()
